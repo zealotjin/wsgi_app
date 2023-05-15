370c45ed8a4cdbce991beace591c50ac7863caec
Jay Turner <jaynicholasturner@gmail.com>
2023-05-15 05:19:18 +0100

    Don't loop over a dictionary twice (#1)

--- app.py
+++ app.py
@@ -85,10 +85,9 @@ class HttpServer(gunicorn.app.base.BaseApplication):
         super().__init__()
 
     def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
+        for key, value in self.options.items():
+            if key in self.cfg.settings and value is not None:
+                self.cfg.set(key.lower(), value)
 
     def load(self):
         return self.application
